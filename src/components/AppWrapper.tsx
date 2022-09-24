c6c3820902c271b72095846ac1257124be71920f
Maheshvar Pandey <mkp10697@gmail.com>
2022-09-25 00:49:27 +0530

    Added the chat options

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -162,7 +162,7 @@ const AppWrapper = ({ children }: any) => {
                                     </li>
                                 </nav>
                             </div>
-                            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+                            {/* <div className="lg:w-1/6 md:w-1/2 w-full px-4">
                                 <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
                                 <nav className="list-none mb-10">
                                     <li>
@@ -178,20 +178,20 @@ const AppWrapper = ({ children }: any) => {
                                         <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
                                     </li>
                                 </nav>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div className="border-t border-gray-200">
                         <div className="container px-5 py-8 flex flex-wrap mx-auto items-center">
                             <div className="flex md:flex-no-wrap flex-wrap justify-center md:justify-start">
-                                <input className="sm:w-64 w-40 bg-gray-100 rounded sm:mr-4 mr-2 border border-gray-400 focus:outline-none focus:border-indigo-500 text-base py-2 px-4" placeholder="Placeholder" type="text" />
-                                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
-                                <p className="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Bitters chicharrones fanny pack
+                                <input className="sm:w-64 w-40 bg-gray-100 rounded sm:mr-4 mr-2 border border-gray-400 focus:outline-none focus:border-indigo-500 text-base py-2 px-4" placeholder="Enter your email" type="text" />
+                                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Submit</button>
+                                {/* <p className="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Bitters chicharrones fanny pack
                                     <br className="lg:block hidden" />waistcoat green juice
-                                </p>
+                                </p> */}
                             </div>
                             <span className="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
-                                <a className="text-gray-500">
+                                {/* <a className="text-gray-500">
                                     <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
                                     </svg>
@@ -200,34 +200,34 @@ const AppWrapper = ({ children }: any) => {
                                     <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
                                     </svg>
-                                </a>
-                                <a className="ml-3 text-gray-500">
+                                </a> */}
+                                <a href='https://instagram.com/unknowncoderss' target="_blank" className="ml-3 text-gray-500" rel="noreferrer">
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
                                         <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
                                         <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
                                     </svg>
                                 </a>
-                                <a className="ml-3 text-gray-500">
+                                {/* <a className="ml-3 text-gray-500">
                                     <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={0} className="w-5 h-5" viewBox="0 0 24 24">
                                         <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
                                         <circle cx={4} cy={4} r={2} stroke="none" />
                                     </svg>
-                                </a>
+                                </a> */}
                             </span>
                         </div>
                     </div>
                     <div className="bg-gray-200">
                         <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
                             <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Unknown Coderss —
-                                <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" target="_blank" rel="noopener noreferrer">@knyttneve</a>
+                                <a href="https://maheshvarpa.web.app" className="text-gray-600 ml-1" target="_blank" rel="noopener noreferrer">@maheshvarpandey</a>
                             </p>
-                            <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Enamel pin tousled raclette tacos irony</span>
+                            {/* <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Enamel pin tousled raclette tacos irony</span> */}
                         </div>
                     </div>
                 </footer>
-                <a href="https://github.com/mertJF/tailblocks" className="rounded-full w-12 h-12 bg-gray-100 fixed bottom-0 right-0 flex items-center justify-center text-gray-800 mr-8 mb-8 shadow-sm border-gray-300 border" target="_blank" rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                {/* <a href="https://github.com/mertJF/tailblocks" className="rounded-full w-12 h-12 bg-gray-100 fixed bottom-0 right-0 flex items-center justify-center text-gray-800 mr-8 mb-8 shadow-sm border-gray-300 border" target="_blank" rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-                </svg></a>
+                </svg></a> */}
             </div>
 
         </div>
